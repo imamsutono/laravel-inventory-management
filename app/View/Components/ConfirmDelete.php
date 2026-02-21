e7da9760da9abc63b44461b1a5e29c85c24e75ba
Imam Sutono <imamssutono@gmail.com>
2026-02-21 23:55:53 +0700

    feat: add confirm delete component

---
+++ app/View/Components/ConfirmDelete.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class ConfirmDelete extends Component
+{
+    public $route;
+
+    public $id;
+
+    /**
+     * Create a new component instance.
+     */
+    public function __construct($route, $id)
+    {
+        $this->route = $route;
+        $this->id = $id;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.confirm-delete');
+    }
+}
